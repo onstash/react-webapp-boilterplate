bb508668243cbca6cc2d32432b45a2ea3a0f9a14
Santosh Venkatraman <santosh.venk@gmail.com>
2017-10-14 13:56:39 +0530

    Adds webpack config to load js & jsx files

--- webpack.config.js
+++ webpack.config.js
@@ -22,4 +22,18 @@ module.exports = {
   // devServer: {
   //   contentBase: paths.SRC
   // }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
