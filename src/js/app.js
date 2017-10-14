c8cc9a8472dc55baebff415631a78198370aee3c
Santosh Venkatraman <santosh.venk@gmail.com>
2017-10-14 13:58:19 +0530

    Adds basic react component

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+        Hello from react
+      </div>
+    );
+  }
+}
+
+render(<HelloWorld />, document.getElementById('app'));
