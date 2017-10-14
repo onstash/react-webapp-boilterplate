89d42e0ab77f4071bae556a5f3d01531df378ea4
Santosh Venkatraman <santosh.venk@gmail.com>
2017-10-14 13:22:48 +0530

    Adds basic index.html for webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
   JS: path.resolve(__dirname, 'src/js')
 };
 
@@ -10,5 +11,8 @@ module.exports = {
   output: {
     path: paths.DIST,
     filename: 'app.bundle.js'
+  },
+  devServer: {
+    contentBase: paths.SRC
   }
 };
